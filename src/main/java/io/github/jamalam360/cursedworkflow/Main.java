c6b0a2adc891e354c7c44f841e3c026f204752f0
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-10-30 21:22:35 +0100

    cursed

---
+++ src/main/java/io/github/jamalam360/cursedworkflow/Main.java
@@ -0,0 +1,10 @@
+package io.github.jamalam360.cursedworkflow;
+
+/**
+ * @author Jamalam360
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Cursed World!");
+    }
+}
